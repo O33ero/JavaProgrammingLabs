f552aa3e638362623a0e50dc4ecd527ad3965344
O33ero <58762519+O33ero@users.noreply.github.com>
2022-02-12 15:08:21 +0300

    #PriorityQueue : Constructors and tests for its

--- src/main/java/pqueue/PriorityQueue.java
+++ src/main/java/pqueue/PriorityQueue.java
@@ -1,4 +1,36 @@
 package pqueue;
 
-public class PriorityQueue {
+import java.util.ArrayList;
+import java.util.List;
+
+public class PriorityQueue<E extends Comparable<E>> {
+    private List<E> heap;
+
+    public PriorityQueue() {
+        heap = new ArrayList<>();
+    }
+
+    public PriorityQueue(int initialCapacity) {
+        heap = new ArrayList<>(initialCapacity);
+    }
+
+    @SuppressWarnings("unchecked")
+    public PriorityQueue(Class<? extends List> listImplementation) {
+        try {
+            heap = listImplementation.getDeclaredConstructor().newInstance();
+        } catch (Exception ex) {
+            throw new IllegalArgumentException("Failed to create new instance of " + listImplementation.getName() + " Message: " + ex);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public PriorityQueue(int initialCapacity, Class<? extends List> listImplementation) {
+        try {
+            heap = listImplementation.getDeclaredConstructor(Integer.TYPE).newInstance(initialCapacity);
+        } catch (Exception ex) {
+            throw new IllegalArgumentException("Failed to create new instance of " + listImplementation.getName() + " Message: " + ex);
+        }
+    }
+
+
 }
--- src/test/java/PriorityQueueTest.java
+++ src/test/java/PriorityQueueTest.java
@@ -1,11 +1,39 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import pqueue.PriorityQueue;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
 
 class PriorityQueueTest {
 
     @Test
     @DisplayName("Should be true")
-    void PriorityQueue_test_0 () {
-
+    void PriorityQueue_Success_0 () {
     }
+
+    @Test
+    @DisplayName("No parameters constructor and constructor with initialCapacity parameter")
+    void PriorityQueue_Constructors_0() {
+        PriorityQueue<Integer> pq0 = new PriorityQueue<>();
+        PriorityQueue<Integer> pq1 = new PriorityQueue<>(100);
+    }
+
+    @Test
+    @DisplayName("Constructors with listImplementation as parameter")
+    void PriorityQueue_Constructors_1() {
+        PriorityQueue<Integer> pq0 = new PriorityQueue<>(ArrayList.class);
+        PriorityQueue<Integer> pq1 = new PriorityQueue<>(LinkedList.class);
+    }
+
+    @Test
+    @DisplayName("LinkedList has not constructor with initialCapacity parameter, expected ")
+    void ProrityQueue_Constructors_2() {
+        Assertions.assertThrows(
+                IllegalArgumentException.class,
+                () -> new PriorityQueue<>(10, LinkedList.class)
+            );
+    }
+
 }
