5c08eae150947b5e3e48f616142466f3605b8fc5
O33ero <58762519+O33ero@users.noreply.github.com>
2022-03-03 10:30:44 +0300

    #Graph : Add DFS function and its test

--- src/main/java/graph/GraphAlgorithmsA.java
+++ src/main/java/graph/GraphAlgorithmsA.java
@@ -1,4 +1,42 @@
 package graph;
 
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class GraphAlgorithmsA {
+
+    public static <T> boolean dfs (Graph<T> graph, T srcVertex, T destVertex) throws GraphException {
+        return dfs(graph, srcVertex, destVertex, new HashSet<T>());
+    }
+    private static <T> boolean dfs(Graph<T> graph, T srcVertex, T destVertex, Set<T> visited) throws GraphException {
+        if (!graph.isContain(srcVertex)) {
+            throw new GraphException("Vertex '" + srcVertex + "' does not exist");
+        }
+        if (!graph.isContain(destVertex)) {
+            throw new GraphException("Vertex '" + srcVertex + "' does not exist");
+        }
+
+        if (srcVertex.equals(destVertex)) {
+            return true;
+        }
+        if (visited.contains(srcVertex)) {
+            return true;
+        }
+
+        visited.add(srcVertex);
+        List<T> neighbors = graph.getOutEdges(srcVertex);
+        for (T neighborVertex : neighbors) {
+            if (!visited.contains(neighborVertex)) {
+                boolean reached = dfs(graph, neighborVertex, destVertex, visited);
+                if (reached) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
 }
--- src/test/java/GraphATest.java
+++ src/test/java/GraphATest.java
@@ -1,3 +1,7 @@
+import graph.Graph;
+import graph.GraphAlgorithmsA;
+import graph.GraphException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -6,5 +10,22 @@ class GraphATest {
     @Test
     @DisplayName("Should be true")
     void GraphA_test_0 () {
+    }
+
+    @Test
+    @DisplayName("Test dfs")
+    void GraphA_dfs_0 () throws GraphException {
+        Graph<Integer> graph = new Graph<>();
+        for(int i = 1; i <= 3000; i++) {
+            graph.addVertex(i);
+        }
+
+        for(int i = 1; i <= 3000; i++) {
+            for (int j = 1; j <= 3000; j++) {
+                graph.addEdge(i, j, i + j);
+            }
+        }
+
+        Assertions.assertTrue(GraphAlgorithmsA.dfs(graph, 1, 3000));
     }
 }
