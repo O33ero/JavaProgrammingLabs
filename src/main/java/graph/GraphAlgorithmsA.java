88ada8bc0d2d51c4a327887977f0e6decf117d48
O33ero <58762519+O33ero@users.noreply.github.com>
2022-03-07 12:58:58 +0300

    #Graph : Changed return value, now returns set of visited vertex. Added new tests

--- src/main/java/graph/GraphAlgorithmsA.java
+++ src/main/java/graph/GraphAlgorithmsA.java
@@ -7,36 +7,28 @@ import java.util.Set;
 
 public class GraphAlgorithmsA {
 
-    public static <T> boolean dfs (Graph<T> graph, T srcVertex, T destVertex) throws GraphException {
-        return dfs(graph, srcVertex, destVertex, new HashSet<T>());
+    private GraphAlgorithmsA() {}
+
+    public static <T> Set<T> dfs (Graph<T> graph, T srcVertex) throws GraphException {
+        return dfs(graph, srcVertex, new HashSet<>());
     }
-    private static <T> boolean dfs(Graph<T> graph, T srcVertex, T destVertex, Set<T> visited) throws GraphException {
+    private static <T> Set<T> dfs(Graph<T> graph, T srcVertex, Set<T> visited) throws GraphException {
         if (!graph.isContain(srcVertex)) {
             throw new GraphException("Vertex '" + srcVertex + "' does not exist");
         }
-        if (!graph.isContain(destVertex)) {
-            throw new GraphException("Vertex '" + srcVertex + "' does not exist");
-        }
-
-        if (srcVertex.equals(destVertex)) {
-            return true;
-        }
         if (visited.contains(srcVertex)) {
-            return true;
+            return visited;
         }
 
         visited.add(srcVertex);
         List<T> neighbors = graph.getOutEdges(srcVertex);
         for (T neighborVertex : neighbors) {
             if (!visited.contains(neighborVertex)) {
-                boolean reached = dfs(graph, neighborVertex, destVertex, visited);
-                if (reached) {
-                    return true;
-                }
+                dfs(graph, neighborVertex, visited);
             }
         }
 
-        return false;
+        return visited;
     }
 
 }
--- src/test/java/GraphATest.java
+++ src/test/java/GraphATest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
 class GraphATest {
 
     @Test
@@ -12,20 +15,54 @@ class GraphATest {
     void GraphA_test_0 () {
     }
 
+
     @Test
-    @DisplayName("Test dfs")
+    @DisplayName("Test graph with 10 elements without edges by DFS")
     void GraphA_dfs_0 () throws GraphException {
         Graph<Integer> graph = new Graph<>();
-        for(int i = 1; i <= 3000; i++) {
+        Set<Integer> expectedSet = new HashSet<>(); // Expected set [1] for vertex "1"
+        expectedSet.add(1);
+
+        for(int i = 1; i <= 10; i++) {
             graph.addVertex(i);
         }
 
-        for(int i = 1; i <= 3000; i++) {
-            for (int j = 1; j <= 3000; j++) {
+        Assertions.assertEquals(expectedSet, GraphAlgorithmsA.dfs(graph, 1));
+    }
+
+    @Test
+    @DisplayName("Test graph with 10 elements with 10 edges by DFS")
+    void GraphA_dfs_1 () throws GraphException {
+        Graph<Integer> graph = new Graph<>();
+        Set<Integer> expectedSet = new HashSet<>(); // Expected set [1, 10] for vertex "1"
+        expectedSet.add(1);
+        expectedSet.add(10);
+
+        for(int i = 1; i <= 10; i++) {
+            graph.addVertex(i);
+        }
+
+        for(int i = 1; i <= 10; i++) {
+            graph.addEdge(i, 10);
+        }
+
+        Assertions.assertEquals(expectedSet, GraphAlgorithmsA.dfs(graph, 1));
+    }
+
+    @Test
+    @DisplayName("Test full-linked graph with 1000 elements by DFS")
+    void GraphA_dfs_2 () throws GraphException {
+        Graph<Integer> graph = new Graph<>();
+        Set<Integer> expectedSet = new HashSet<>(); // Expected set [1, 2, ... 1000] for vertex "1"
+        for(int i = 1; i <= 1000; i++) {
+            expectedSet.add(i);
+            graph.addVertex(i);
+        }
+        for(int i = 1; i <= 1000; i++) {
+            for (int j = 1; j <= 1000; j++) {
                 graph.addEdge(i, j, i + j);
             }
         }
-
-        Assertions.assertTrue(GraphAlgorithmsA.dfs(graph, 1, 3000));
+        Assertions.assertEquals(expectedSet, GraphAlgorithmsA.dfs(graph, 1));
     }
 }
