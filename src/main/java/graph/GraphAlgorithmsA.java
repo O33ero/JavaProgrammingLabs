31f4e9769f2666965b8f99f33c0435fdf09218ca
O33ero <58762519+O33ero@users.noreply.github.com>
2022-03-07 14:45:18 +0300

    #Graph : Add BFS and its tests

--- src/main/java/graph/GraphAlgorithmsA.java
+++ src/main/java/graph/GraphAlgorithmsA.java
@@ -14,7 +14,7 @@ public class GraphAlgorithmsA {
         }
 
         Stack<T> stack = new Stack<>();
-        Set<T> visited = new HashSet<>();
+        Set<T> visited = new LinkedHashSet<>();
         stack.push(srcVertex);
         while (!stack.isEmpty()) {
             T cur = stack.pop();
@@ -25,6 +25,31 @@ public class GraphAlgorithmsA {
                 for (T neighborVertex : neighbors) {
                     if (!visited.contains(neighborVertex)) {
                         stack.add(neighborVertex);
+                    }
+                }
+            }
+        }
+
+        return visited;
+    }
+
+    public static <T> Set<T> bfs(Graph<T> graph, T srcVertex) throws GraphException {
+        if (!graph.isContain(srcVertex)) {
+            throw new GraphException("Vertex '" + srcVertex + "' does not exist");
+        }
+        Queue<T> queue = new LinkedList<>();
+        Set<T> visited = new LinkedHashSet<>();
+
+        queue.add(srcVertex);
+
+        while (!queue.isEmpty()) {
+            T cur = queue.poll();
+            if (!visited.contains(cur)) {
+                visited.add(cur);
+                List<T> neighbors = graph.getOutEdges(srcVertex);
+                for (T neighborVertex : neighbors) {
+                    if (!visited.contains(neighborVertex)) {
+                        queue.add(neighborVertex);
                     }
                 }
             }
--- src/test/java/GraphATest.java
+++ src/test/java/GraphATest.java
@@ -65,4 +65,54 @@ class GraphATest {
         }
         Assertions.assertEquals(expectedSet, GraphAlgorithmsA.dfs(graph, 1));
     }
+
+    @Test
+    @DisplayName("Test graph with 10 elements without edges by BFS")
+    void GraphA_bfs_0 () throws GraphException {
+        Graph<Integer> graph = new Graph<>();
+        Set<Integer> expectedSet = new HashSet<>(); // Expected set [1] for vertex "1"
+        expectedSet.add(1);
+
+        for(int i = 1; i <= 10; i++) {
+            graph.addVertex(i);
+        }
+
+        Assertions.assertEquals(expectedSet, GraphAlgorithmsA.bfs(graph, 1));
+    }
+
+    @Test
+    @DisplayName("Test graph with 10 elements with 10 edges by BFS")
+    void GraphA_bfs_1 () throws GraphException {
+        Graph<Integer> graph = new Graph<>();
+        Set<Integer> expectedSet = new HashSet<>(); // Expected set [1, 10] for vertex "1"
+        expectedSet.add(1);
+        expectedSet.add(10);
+
+        for(int i = 1; i <= 10; i++) {
+            graph.addVertex(i);
+        }
+
+        for(int i = 1; i <= 10; i++) {
+            graph.addEdge(i, 10);
+        }
+
+        Assertions.assertEquals(expectedSet, GraphAlgorithmsA.bfs(graph, 1));
+    }
+
+    @Test
+    @DisplayName("Test full-linked graph with 1000 elements by BFS")
+    void GraphA_bfs_2 () throws GraphException {
+        Graph<Integer> graph = new Graph<>();
+        Set<Integer> expectedSet = new HashSet<>(); // Expected set [1, 2, ... 1000] for vertex "1"
+        for(int i = 1; i <= 1000; i++) {
+            expectedSet.add(i);
+            graph.addVertex(i);
+        }
+        for(int i = 1; i <= 1000; i++) {
+            for (int j = 1; j <= 1000; j++) {
+                graph.addEdge(i, j, i + j);
+            }
+        }
+        Assertions.assertEquals(expectedSet, GraphAlgorithmsA.bfs(graph, 1));
+    }
 }
