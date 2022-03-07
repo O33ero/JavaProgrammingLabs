15b5302c1d421870bf79e92740bbe53a65f64869
O33ero <58762519+O33ero@users.noreply.github.com>
2022-03-07 19:26:16 +0300

    #Graph : Cleaning tests

--- src/test/java/GraphATest.java
+++ src/test/java/GraphATest.java
@@ -12,18 +12,18 @@ class GraphATest {
 
     @Test
     @DisplayName("Should be true")
-    void GraphA_test_0 () {
+    void GraphA_test_0() {
     }
 
 
     @Test
     @DisplayName("Test graph with 10 elements without edges by DFS")
-    void GraphA_dfs_0 () throws GraphException {
+    void GraphA_dfs_0() throws GraphException {
         Graph<Integer> graph = new Graph<>();
         Set<Integer> expectedSet = new HashSet<>(); // Expected set [1] for vertex "1"
         expectedSet.add(1);
 
-        for(int i = 1; i <= 10; i++) {
+        for (int i = 1; i <= 10; i++) {
             graph.addVertex(i);
         }
 
@@ -32,17 +32,17 @@ class GraphATest {
 
     @Test
     @DisplayName("Test graph with 10 elements with 10 edges by DFS")
-    void GraphA_dfs_1 () throws GraphException {
+    void GraphA_dfs_1() throws GraphException {
         Graph<Integer> graph = new Graph<>();
         Set<Integer> expectedSet = new HashSet<>(); // Expected set [1, 10] for vertex "1"
         expectedSet.add(1);
         expectedSet.add(10);
 
-        for(int i = 1; i <= 10; i++) {
+        for (int i = 1; i <= 10; i++) {
             graph.addVertex(i);
         }
 
-        for(int i = 1; i <= 10; i++) {
+        for (int i = 1; i <= 10; i++) {
             graph.addEdge(i, 10);
         }
 
@@ -51,14 +51,14 @@ class GraphATest {
 
     @Test
     @DisplayName("Test full-linked graph with 1000 elements by DFS")
-    void GraphA_dfs_2 () throws GraphException {
+    void GraphA_dfs_2() throws GraphException {
         Graph<Integer> graph = new Graph<>();
         Set<Integer> expectedSet = new HashSet<>(); // Expected set [1, 2, ... 1000] for vertex "1"
-        for(int i = 1; i <= 1000; i++) {
+        for (int i = 1; i <= 1000; i++) {
             expectedSet.add(i);
             graph.addVertex(i);
         }
-        for(int i = 1; i <= 1000; i++) {
+        for (int i = 1; i <= 1000; i++) {
             for (int j = 1; j <= 1000; j++) {
                 graph.addEdge(i, j, i + j);
             }
@@ -68,12 +68,12 @@ class GraphATest {
 
     @Test
     @DisplayName("Test graph with 10 elements without edges by BFS")
-    void GraphA_bfs_0 () throws GraphException {
+    void GraphA_bfs_0() throws GraphException {
         Graph<Integer> graph = new Graph<>();
         Set<Integer> expectedSet = new HashSet<>(); // Expected set [1] for vertex "1"
         expectedSet.add(1);
 
-        for(int i = 1; i <= 10; i++) {
+        for (int i = 1; i <= 10; i++) {
             graph.addVertex(i);
         }
 
@@ -82,17 +82,17 @@ class GraphATest {
 
     @Test
     @DisplayName("Test graph with 10 elements with 10 edges by BFS")
-    void GraphA_bfs_1 () throws GraphException {
+    void GraphA_bfs_1() throws GraphException {
         Graph<Integer> graph = new Graph<>();
         Set<Integer> expectedSet = new HashSet<>(); // Expected set [1, 10] for vertex "1"
         expectedSet.add(1);
         expectedSet.add(10);
 
-        for(int i = 1; i <= 10; i++) {
+        for (int i = 1; i <= 10; i++) {
             graph.addVertex(i);
         }
 
-        for(int i = 1; i <= 10; i++) {
+        for (int i = 1; i <= 10; i++) {
             graph.addEdge(i, 10);
         }
 
@@ -101,14 +101,14 @@ class GraphATest {
 
     @Test
     @DisplayName("Test full-linked graph with 1000 elements by BFS")
-    void GraphA_bfs_2 () throws GraphException {
+    void GraphA_bfs_2() throws GraphException {
         Graph<Integer> graph = new Graph<>();
         Set<Integer> expectedSet = new HashSet<>(); // Expected set [1, 2, ... 1000] for vertex "1"
-        for(int i = 1; i <= 1000; i++) {
+        for (int i = 1; i <= 1000; i++) {
             expectedSet.add(i);
             graph.addVertex(i);
         }
-        for(int i = 1; i <= 1000; i++) {
+        for (int i = 1; i <= 1000; i++) {
             for (int j = 1; j <= 1000; j++) {
                 graph.addEdge(i, j, i + j);
             }
@@ -117,7 +117,8 @@ class GraphATest {
     }
 
     @Test
-    @DisplayName("Test #1 algorithm Dijkstra's") // https://www.baeldung.com/wp-content/uploads/2017/01/step8.png
+    @DisplayName("Test #1 algorithm Dijkstra's")
+        // https://www.baeldung.com/wp-content/uploads/2017/01/step8.png
     void GraphA_dijkstra_0() throws GraphException {
         Graph<String> graph = new Graph<>();
 
@@ -132,7 +133,7 @@ class GraphATest {
         graph.addEdge("A", "C", 15);
         graph.addEdge("B", "F", 15);
         graph.addEdge("B", "D", 12);
-        graph.addEdge("C", "E",10);
+        graph.addEdge("C", "E", 10);
         graph.addEdge("D", "F", 1);
         graph.addEdge("D", "E", 2);
         graph.addEdge("F", "E", 5);
@@ -141,7 +142,8 @@ class GraphATest {
     }
 
     @Test
-    @DisplayName("Test #2 algorithm Dijkstra's") // https://s3.ap-south-1.amazonaws.com/s3.studytonight.com/tutorials/uploads/pictures/1627292679-103268.png
+    @DisplayName("Test #2 algorithm Dijkstra's")
+        // https://s3.ap-south-1.amazonaws.com/s3.studytonight.com/tutorials/uploads/pictures/1627292679-103268.png
     void GraphA_dijkstra_1() throws GraphException {
         Graph<Integer> graph = new Graph<>();
 
@@ -176,7 +178,7 @@ class GraphATest {
     void GraphA_dijkstra_3() throws GraphException {
         Graph<Integer> graph = new Graph<>();
 
-        for(int i = 1; i <= 10; i++) {
+        for (int i = 1; i <= 10; i++) {
             graph.addVertex(i);
         }
 
@@ -188,11 +190,11 @@ class GraphATest {
     void GraphA_dijkstra_4() throws GraphException {
         Graph<Integer> graph = new Graph<>();
 
-        for(int i = 1; i <= 100; i++) {
+        for (int i = 1; i <= 100; i++) {
             graph.addVertex(i);
         }
 
-        for(int i = 1; i <= 100; i++) {
+        for (int i = 1; i <= 100; i++) {
             for (int j = 1; j <= 100; j++) {
                 graph.addEdge(i, j, i + j);
             }
@@ -206,11 +208,11 @@ class GraphATest {
     void GraphA_dijkstra_5() throws GraphException {
         Graph<Integer> graph = new Graph<>();
 
-        for(int i = 1; i <= 1000; i++) {
+        for (int i = 1; i <= 1000; i++) {
             graph.addVertex(i);
         }
 
-        for(int i = 1; i <= 1000; i++) {
+        for (int i = 1; i <= 1000; i++) {
             for (int j = 1; j <= 1000; j++) {
                 graph.addEdge(i, j, i + j);
             }
@@ -225,11 +227,11 @@ class GraphATest {
     void GraphA_dijkstra_6() throws GraphException {
         Graph<Integer> graph = new Graph<>();
 
-        for(int i = 1; i <= 5000; i++) {
+        for (int i = 1; i <= 5000; i++) {
             graph.addVertex(i);
         }
 
-        for(int i = 1; i <= 5000; i++) {
+        for (int i = 1; i <= 5000; i++) {
             for (int j = 1; j <= 5000; j++) {
                 graph.addEdge(i, j, i + j);
             }
