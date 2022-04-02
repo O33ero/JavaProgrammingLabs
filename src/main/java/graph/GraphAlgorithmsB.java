c90b082babbff8c2a13b428207f13dee89d953f7
O33ero <58762519+O33ero@users.noreply.github.com>
2022-04-02 15:09:52 +0300

    #Graph : Tarjan's algorithm and test

--- src/main/java/graph/GraphAlgorithmsB.java
+++ src/main/java/graph/GraphAlgorithmsB.java
@@ -1,4 +1,46 @@
 package graph;
 
+import java.util.*;
+
 public class GraphAlgorithmsB {
+    private GraphAlgorithmsB() {
+    }
+
+    /**
+     * Алгоритм Тарьяна для топологической сортровки.
+     * Топологический порядок вершин - такой порядок, при котором ребра вершин направлены строго в одном направлении.
+     * То есть в списке вершин [a, b, c] может существовать ребро (a, b), (a, c) или (b, c), но точно не существует ребра (b, a), (c, a), или (c, b).
+     * @param graph Граф
+     * @param <T> Тип данных имен вершин
+     * @return Списко в топологическом порядке.
+     * @throws GraphException Исключение, если в графе найден цикл.
+     */
+    public static <T> List<T> tarjan(Graph<T> graph) throws GraphException {
+        Set<T> grayVertex = new HashSet<>();
+        List<T> blackVertex = new ArrayList<>();
+
+        for (T vertex : graph.getVertexNames()) {
+            helperTarjan(vertex, graph, blackVertex, grayVertex);
+        }
+
+        Collections.reverse(blackVertex);
+        return blackVertex;
+    }
+
+    private static <T> void helperTarjan(T vertex, Graph<T> graph, List<T> blackVertex, Set<T> grayVertex) throws GraphException {
+        if (blackVertex.contains(vertex)) {
+            return;
+        }
+        if (grayVertex.contains(vertex)) {
+            throw new GraphException("Detected loop. Algorithm cannot be used for this graph.");
+        }
+
+        grayVertex.add(vertex);
+        List<T> neighbors = graph.getOutEdges(vertex);
+        for (T neighbor : neighbors) {
+            helperTarjan(neighbor, graph, blackVertex, grayVertex);
+        }
+        grayVertex.remove(vertex);
+        blackVertex.add(vertex);
+    }
 }
--- src/test/java/GraphBTest.java
+++ src/test/java/GraphBTest.java
@@ -1,11 +1,99 @@
+import graph.Graph;
+import graph.GraphAlgorithmsB;
+import graph.GraphException;
+import graph.MatrixGraph;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 class GraphBTest {
 
     @Test
     @DisplayName("Should be true")
-    void GraphB_test_0 () {
+    void GraphB_test_0() {
+    }
 
+    @Test
+    @DisplayName("Tarjan algorithm. Simple test")
+    void GraphB_tarjan_0() throws GraphException {
+        Graph<Integer> graph = new MatrixGraph<>();
+
+        graph.addVertex(0);
+        graph.addVertex(1);
+        graph.addVertex(2);
+        graph.addVertex(3);
+        graph.addVertex(4);
+
+        graph.addEdge(0, 2);
+        graph.addEdge(0, 1);
+        graph.addEdge(1, 2);
+        graph.addEdge(1, 3);
+        graph.addEdge(2, 3);
+        graph.addEdge(2, 4);
+        graph.addEdge(3, 4);
+
+        List<Integer> expect = new ArrayList<>();
+        expect.add(0);
+        expect.add(1);
+        expect.add(2);
+        expect.add(3);
+        expect.add(4);
+
+        Assertions.assertEquals(expect, GraphAlgorithmsB.tarjan(graph));
+    }
+
+
+    @Test
+    @DisplayName("Tarjan algorithm. Simple test in shuffle order")
+    void GraphB_tarjan_1() throws GraphException {
+        Graph<Integer> graph = new MatrixGraph<>();
+        graph.addVertex(3);
+        graph.addVertex(0);
+        graph.addVertex(1);
+        graph.addVertex(4);
+        graph.addVertex(2);
+
+        graph.addEdge(0, 2);
+        graph.addEdge(0, 1);
+        graph.addEdge(1, 2);
+        graph.addEdge(1, 3);
+        graph.addEdge(2, 3);
+        graph.addEdge(2, 4);
+        graph.addEdge(3, 4);
+
+        List<Integer> expect = new ArrayList<>();
+        expect.add(0);
+        expect.add(1);
+        expect.add(2);
+        expect.add(3);
+        expect.add(4);
+
+        Assertions.assertEquals(expect, GraphAlgorithmsB.tarjan(graph));
+    }
+
+    @Test
+    @DisplayName("Tarjan algorithm. Test with loop")
+    void GraphB_tarjan_2() {
+        Graph<Integer> graph = new MatrixGraph<>();
+        graph.addVertex(0);
+        graph.addVertex(1);
+        graph.addVertex(2);
+        graph.addVertex(3);
+        graph.addVertex(4);
+
+        graph.addEdge(0, 2);
+        graph.addEdge(0, 1);
+        graph.addEdge(1, 2);
+        graph.addEdge(1, 3);
+        graph.addEdge(2, 3);
+        graph.addEdge(2, 4);
+        graph.addEdge(3, 4);
+
+        graph.addEdge(1, 1); // loop
+
+        Assertions.assertThrows(GraphException.class, () -> GraphAlgorithmsB.tarjan(graph));
     }
 }
